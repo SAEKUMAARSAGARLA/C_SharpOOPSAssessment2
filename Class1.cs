7d3aa4ec330d452612bf2d06b11048b6f372ead0
SAEKUMAARSAGARLA <102402164+SAEKUMAARSAGARLA@users.noreply.github.com>
2022-04-08 09:56:45 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1
+{ 
+    public class employee
+    {
+
+        // Instance Variables
+        int id;
+        String name;
+        int salary;
+
+        // Constructor Declaration of Class
+        public employee(int id,String name, int salary)
+        {
+            this.id = id;
+            this.name = name;
+           
+            this.salary = salary;
+        }
+       
+        public int Getid()
+        {
+            return id;
+        }
+       
+        public String GetName()
+        {
+            return name;
+        }
+
+        public int Getsalary()
+        {
+            return salary;
+        }
+
+        public String ToString()
+        {
+            return ("Hi my name is " + this.GetName()
+                    + ".\nMy id, age and salary are " + this.Getid()
+                    + "," + this.Getsalary());
+        }
+        public static void Main(String[] args)
+        {
+
+            employee saekumaar = new employee( 1001,"saekumaar", 20000);
+            Console.WriteLine(saekumaar.ToString());
+        }
+    }
+}
